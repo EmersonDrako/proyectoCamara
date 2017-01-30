52dd5fb0eabd849d4a9397ba4a946cbce48204bd
EmersonDrako <epena28@misena.edu.co>
2017-01-30 15:27:46 -0500

    hola mundo pruebas para azure

--- app.js
+++ app.js
@@ -1,46 +1,38 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require("express");
+var app = new express();
+var http= require("http").Server(app);
+var io = require("socket.io")(http);
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var Log = require('log'),
+    log = new Log('debug')
 
-var app = express();
+var port = process.env.PORT ||  80;
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.use(express.static(__dirname + "/public" ));
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.get('/',function(req,res){
+	res.redirect('index.html');
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+io.on('connection',function(socket){
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	socket.on('stream',function(image){
+socket.broadcast.emit('stream', image);
+	});
 });
 
-module.exports = app;
+ var visitas = 0;
+ io.on('connection', function(socket){
+   visitas++;
+   socket.emit('user2',{number: visitas});
+   socket.broadcast.emit('user2',{number: visitas});
+   socket.on('disconnect', function(){
+     visitas--;
+     socket.broadcast.emit('user2',{number: visitas});
+      socket.broadcast.emit('user2', {number: visitas});
+   });
+ });
+
+http.listen(port,function(){
+log.info('servidor escuchando por el puerto %s', port);
+});
