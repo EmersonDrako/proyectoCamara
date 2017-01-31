0dfc4b1a2ed75d5f202b4fb8855749185f498968
EmersonDrako <epena28@misena.edu.co>
2017-01-30 21:33:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = new express();
 var http= require("http").Server(app);
 var io = require("socket.io")(http);
+
 var Log = require('log'),
     log = new Log('debug')
 
@@ -13,7 +14,12 @@ app.get('/',function(req,res){
 	res.redirect('index.html');
 });
 
+io.on('connection',function(socket){
 
+	socket.on('stream',function(image){
+socket.broadcast.emit('stream', image);
+	});
+});
 
  var visitas = 0;
  io.on('connection', function(socket){
@@ -30,17 +36,3 @@ app.get('/',function(req,res){
 http.listen(port,function(){
 log.info('servidor escuchando por el puerto %s', port);
 });
-
-io.on('connection', function(socket){
-  
-  socket.on('stream', function(data){
-
-        socket.emit('stream',data);
-    socket.broadcast.emit('stream',data);
-  });
-
-  socket.on('disconnect', function () {
-      
-    });
-});
- 
