6391fffb20c9ee861ccbfc5533535ea3e74cda10
EmersonDrako <epena28@misena.edu.co>
2017-03-11 12:44:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ socket.broadcast.emit('stream', image);
 
  var visitas = 0;
  io.on('connection', function(socket){
-	   console.log(clientIp);
+	  var clientIp = socket.request.connection.remoteAddress;
       socket.broadcast.emit('prueba1', {ip: clientIp});
    visitas++;
    socket.emit('user2',{number: visitas});
